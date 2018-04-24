a6f328760fc55de427bcc7f6605721efc74354a5
luisamachado <luisamachado@gmail.com>
2018-04-24 10:26:25 -0300

    Comentando run-to-abs-pos e ativando run_timed

--- movimento.py
+++ movimento.py
@@ -23,38 +23,40 @@ class movimento(object):
     while not self.ts.value():
       angle = self.gy.value()
       print(str(angle) + " " + self.units)
-      #self.mA.run_timed(time_sp = 500, speed_sp = 80)
-      self.mA.run-to-abs-pos(posicion_sp = 90)
+      self.mA.run_timed(time_sp = 500, speed_sp = 80)
+      #self.mA.run-to-abs-pos(posicion_sp = 90)
       if angle >= 85:
         self.mA.stop(stop_action = "brake")
         break
       sleep(0.25)
-      #andar(self.steps)
-      #alinhar(2)
+      self.andar()
+      self.alinhar(2)
     
   def sul(self):
     while not self.ts.value():
       angle = self.gy.value()
       print(str(angle) + " " + self.units)
-      #self.mA.run_timed(time_sp = 500, speed_sp = 80)
-      self.mA.run-to-abs-pos(posicion_sp = 180)
+      self.mA.run_timed(time_sp = 500, speed_sp = 80)
+      #self.mA.run-to-abs-pos(posicion_sp = 180)
       if angle >= 174:
         self.mA.stop(stop_action = "brake")
         break
       sleep(0.25)
-      #alinhar(3)
+      self.andar()
+      self.alinhar(3)
     
   def leste(self):
     while not self.ts.value():
       print("Virou para o leste")
       angle = self.gy.value()
       print(str(angle) + " " + self.units)
-      #self.mD.run_timed(time_sp = 500, speed_sp = 80)
-      self.mD.run-to-abs-pos(posicion_sp = 90)
+      self.mD.run_timed(time_sp = 500, speed_sp = 80)
+      #self.mD.run-to-abs-pos(posicion_sp = 90)
       if angle <= -85:
         self.mD.stop(stop_action = "brake")
         break
-      #alinhar(4)
+      self.andar()
+      self.alinhar(4)
          
   def andar(self):
     for x in range(0, self.steps):
@@ -69,8 +71,8 @@ class movimento(object):
         print("Virou para o leste")
         angle = self.gy.value()
         print(str(angle) + " " + self.units)
-        self.mD.run-to-abs-pos(posicion_sp = 90)
-        #self.mD.run_timed(time_sp = 500, speed_sp = 80)
+        #self.mD.run-to-abs-pos(posicion_sp = 90)
+        self.mD.run_timed(time_sp = 500, speed_sp = 80)
         if angle <= 1:
           self.mD.stop(stop_action = "brake")
           break
@@ -78,7 +80,7 @@ class movimento(object):
       while not self.ts.value():
         angle = self.gy.value()
         print(str(angle) + " " + self.units)
-        self.mA.run-to-abs-pos(posicion_sp = 90)
+        #self.mA.run-to-abs-pos(posicion_sp = 90)
         self.mA.run_timed(time_sp = 500, speed_sp = 80)
         if angle >= 360:
           self.mA.stop(stop_action = "brake")
@@ -87,8 +89,8 @@ class movimento(object):
       while not self.ts.value():
         angle = self.gy.value()
         print(str(angle) + " " + self.units)
-        self.mA.run-to-abs-pos(posicion_sp = 180)
-        #self.mA.run_timed(time_sp = 500, speed_sp = 80)
+        #self.mA.run-to-abs-pos(posicion_sp = 180)
+        self.mA.run_timed(time_sp = 500, speed_sp = 80)
         if angle >= -1:
           self.mA.stop(stop_action = "brake")
           break
