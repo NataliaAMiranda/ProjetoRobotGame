3c9a295f583abf3ea2eae78a02c72df705cfbc6e
Marina-Souza <38480467+Marina-Souza@users.noreply.github.com>
2018-04-29 22:43:37 -0300

    Update webserv.py

--- webserv.py
+++ webserv.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python
-#!flask/bin/python
+  #!/usr/bin/python
 from flask import Flask, jsonify
 from flask import abort
-from flask_cors import CORS, cross_origin
 from flask import make_response
+from flask_cors import CORS, cross_origin
 from flask import request
 from flask import url_for
 from flask.ext.httpauth import HTTPBasicAuth
@@ -11,14 +10,15 @@ from flask.ext.httpauth import HTTPBasicAuth
 auth = HTTPBasicAuth()
 
 app = Flask(__name__)
+CORS(app)
 
 oper = [
           {
-	  'id': 1,
-          'dir' : 1,
-          'steps' : 1
-	  #'novojogo': True,
-	  #'novajogada': True
+          'id': 1,
+          'dir' : 0,
+          'steps' : 0,
+          'novojogo': True,
+          'novajogada': True
           },
 ]
 
@@ -31,10 +31,28 @@ def atualizar_valores():
     resultado = [resultado for resultado in oper if resultado['id'] == 1]
     resultado[0]['dir'] = request.json.get('dir', resultado[0]['dir'])
     resultado[0]['steps'] = request.json.get('steps', resultado[0]['steps'])
-    print "atualizou"
+    resultado[0]['novajogada'] = request.json.get('novajogada',resultado[0]['novajogada'])
+    print('atualizou')
     return jsonify({'ok': '1'}), 201
+
+@app.route('/finalizajogada', methods=['GET'])
+def finaliza_jogada():
+    resultado = [resultado for resultado in oper if resultado['id'] == 1]
+    resultado[0]['novajogada'] = False
+    return jsonify({'ok': '1'}), 201
+
+@app.route('/finalizapartida', methods=['GET'])
+def finaliza_partida():
+    resultado = [resultado for resultado in oper if resultado['id'] == 1]
+    resultado[0]['novojogo'] = False
+    return jsonify({'ok': '1'}), 201
+
+
 if __name__ == "__main__":
-    print "Servidor no ar!"
+    print('Servidor no ar!')
     app.run(host='0.0.0.0', debug=True)
+
+
+
 
 
