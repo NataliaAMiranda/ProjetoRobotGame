9e0be5984c86e1ad0c162e3430202b889b7b56d2
Marina-Souza <38480467+Marina-Souza@users.noreply.github.com>
2018-04-29 22:41:08 -0300

    Update play.py

--- play.py
+++ play.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-import sys
-sys.path.append('../')
 from movimento import *
 from ev3dev.ev3 import *
 from time import sleep
@@ -8,23 +5,35 @@ import json
 import requests
 
 
-response = requests.get("http://192.168.0.7:5000/oper").text
+response = requests.get("http://localhost:5000/oper").text
 response = json.loads(response)
-print (response)
 dire = response['oper'][0]['dir']
 steps = response ['oper'][0]['steps']
-print (dire)
-print(steps)
-#novajogada = True
-#novojogo = True
+novajogada = response['oper'][0]['novajogada']
+novojogo = response['oper'][0]['novojogo']
 
-mov = movimento(dire,steps)
-if dire == 2:
-  mov.oeste()
-elif dire == 3:
-	mov.leste()
-elif dire == 1:
-  mov.norte()
-else: mov.sul()    
-mov.alinhar()
+while (novojogo == True):
+ response = requests.get("http://localhost:5000/oper").text
+ response = json.loads(response)
+ dire = response['oper'][0]['dir']
+ steps = response ['oper'][0]['steps']
+ novajogada = response['oper'][0]['novajogada']
+ novojogo = response['oper'][0]['novojogo']
+ if novajogada == True:
+  mov = movimento(dire,steps)
+  if dire == 2:
+   mov.oeste()
+   mov.andar()
+  elif dire == 3:
+   mov.leste()
+   mov.andar()
+  elif dire == 1:
+   mov.norte()
+  else:
+   mov.sul()
+   mov.andar()
+  mov.alinhar()
+  response = requests.get("http://localhost:5000/finalizajogada").text
+ else:
+  pass
 
