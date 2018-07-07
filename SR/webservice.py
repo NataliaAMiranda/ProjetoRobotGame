76b461d88cf239703f00e1866c459dff21827b56
Marina-Souza <38480467+Marina-Souza@users.noreply.github.com>
2018-07-07 16:24:14 -0300

    Update webservice.py

--- SR/webservice.py
+++ SR/webservice.py
@@ -1,23 +1,16 @@
-#!/usr/bin/pythonfrom
+ #!/usr/bin/pythonfrom
 from flask import Flask, jsonify
-from flask import abort
-from flask import make_response
-from flask_cors import CORS, cross_origin
 from flask import request
-from flask import url_for
-from flask_httpauth import HTTPBasicAuth
-from manual import *
-
-auth = HTTPBasicAuth()
 
 app = Flask(__name__)
-CORS(app)
+
 
 oper = [
           {
           'id': 1,
           'dir' : 0,
           'steps' : 0,
+          'caca':[[1,2],[4,2],[6,7],[9,1],[2,3],[6,7],[9,1],[2,3]],
           },
 ]
 
@@ -28,16 +21,26 @@ def obtem_oper():
 @app.route('/atualiza', methods=['PUT'])
 def atualizar_valores():
     resultado = [resultado for resultado in oper if resultado['id'] == 1]
-    resultado[0]['dir'] = request.json.get('dir', resultado[0]['dir'])
+    resultado[0]['dir'] = request.json    
+    print("Alterou") .get('dir', resultado[0]['dir'])
     resultado[0]['steps'] = request.json.get('steps', resultado[0]['steps'])
     dire = request.json.get('dir', resultado[0]['dir'])
     steps = request.json.get('steps', resultado[0]['steps'])
-    #mov = manual(dire,steps)
-    #mov.movimento()
-    print(steps)
-    print(dire)
+
+    mov = manual(dire,steps)
+    mov.movimento()
+
+    print('atualizou')
+    return jsonify({'ok': '1'}), 201
+    
+@app.route('/automatico', methods=['PUT'])
+def automatico():
+    resultado = [resultado for resultado in oper if resultado['id'] == 1]
+    resultado[0]['caca'] = request.json.get('caca', resultado[0]['caca'])  
+
     return jsonify({'ok': '1'}), 201
 
 if __name__ == "__main__":
     print('Servidor no ar!')
     app.run(host='0.0.0.0', port='5040', debug=True)
+
